bef9b79b1e29dddf41c25ceccdfc6ad9e6fce1ba
MarkovGerman <lllkdodobdvakk@gmail.com>
2025-03-16 22:21:10 +0500

    Module 1 task 2

---
+++ src/cli/commands/import.command.ts
@@ -0,0 +1,20 @@
+import {TsvFileReader} from '../../file-reader/tsv-file-reader.js';
+import {Command} from './command.interface.js';
+
+export class ImportCommand implements Command {
+  public readonly name = '--import';
+
+  public execute(filename: string): void {
+    const fileReader = new TsvFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        throw err;
+      }
+      console.log(`Не удалось импортировать данные из файла: '${filename}'`);
+    }
+  }
+}
