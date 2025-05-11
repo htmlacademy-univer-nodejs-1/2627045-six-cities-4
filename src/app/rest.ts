e63ae826f4ea94e7fd08bdd7b3ce1a667cc5f17c
MarkovGerman <lllkdodobdvakk@gmail.com>
2025-05-11 22:29:49 +0500

    Add logger

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -1,13 +1,17 @@
 import {inject, injectable} from 'inversify';
-import { AppComponent } from '../shared/types/app-component.js';
-import { RestSchema } from '../shared/config/rest.shema.js';
 import { ConfigInterface } from '../shared/config/config.interface.js';
+import { LoggerInterface } from '../shared/logger/logger.interface.js';
+import { AppComponent } from '../shared/types/app-component.enum.js';
+import { RestSchema } from '../shared/config/rest.shema.js';
 
 @injectable()
 export default class Application {
-  constructor(@inject(AppComponent.ConfigInterface) private readonly config: ConfigInterface<RestSchema>) {
+  constructor(@inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface,
+              @inject(AppComponent.ConfigInterface) private readonly config: ConfigInterface<RestSchema>) {
   }
 
   public async init() {
+    this.logger.info('Application init');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
--- src/shared/config/config.service.ts
+++ src/shared/config/config.service.ts
@@ -1,19 +1,23 @@
 import {config} from 'dotenv';
+import { LoggerInterface } from '../logger/logger.interface.js';
 import { ConfigInterface } from './config.interface.js';
-import { injectable } from 'inversify';
+import {inject, injectable} from 'inversify';
 import { configRestSchema, RestSchema } from './rest.shema.js';
+import { AppComponent } from '../types/app-component.enum.js';
 
 @injectable()
 export default class ConfigService implements ConfigInterface<RestSchema> {
   private readonly config: RestSchema;
 
-  constructor() {
+  constructor(@inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface) {
     const parsedOutput = config();
     if (parsedOutput.error) {
       throw new Error('Can\'t read .env file');
     }
     configRestSchema.load({});
+    configRestSchema.validate({allowed: 'strict', output: this.logger.info});
     this.config = configRestSchema.getProperties();
+    this.logger.info('.env file found and successfully parsed');
   }
 
   public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
